95534d48af40fa8cf97657ecd01bcd92cfbe8aa4
Zoltan Sasvari <sasvarizoli@me.com>
2021-10-13 17:04:25 +0200

    secure attendance endpoints

--- src/main/java/com/studo/services/attendance/rest/AttendanceRestService.java
+++ src/main/java/com/studo/services/attendance/rest/AttendanceRestService.java
@@ -9,13 +9,17 @@ import com.studo.services.attendance.entity.study.StudyEntity;
 import com.studo.services.attendance.service.OrganisationService;
 import com.studo.services.attendance.service.CourseService;
 import com.studo.services.attendance.service.UserService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.resteasy.annotations.GZIP;
 
+import javax.annotation.security.RolesAllowed;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Objects;
 
 /**
  *
@@ -38,16 +42,27 @@ public class AttendanceRestService {
     @Inject
     UserService userService;
 
+    @ConfigProperty(name = "studo-service.token-secret")
+    String secret;
+
+    private void checkAuthorizationHeader(String secret) {
+        if (!Objects.equals(secret, this.secret.substring(0,32))) {
+            throw new SecurityException("Invalid authorization header");
+        }
+    }
+
     @GET
     public AttendanceDto getAttendance(
             @DefaultValue("1") @QueryParam(value = "orgId") List<BigDecimal> orgIds,
             @DefaultValue("2021/22") @QueryParam(value = "academicYear") String academicYear,
             @DefaultValue("W") @QueryParam(value = "semester") List<String> semesters,
             @DefaultValue("FT") @QueryParam(value = "resourceType") List<String> resourceTypes,
-            @DefaultValue("A") @QueryParam(value = "eventType") List<String> eventTypes
+            @DefaultValue("A") @QueryParam(value = "eventType") List<String> eventTypes,
+            @HeaderParam(value = "X-Authorization") String secret
     ) {
-
+        checkAuthorizationHeader(secret);
         List<CourseEntity> courseEntities = courseService.getCourseEntities(orgIds, academicYear, semesters);
+
         return new AttendanceDto(
                 courseService.getCourses(courseEntities, resourceTypes, eventTypes),
                 userService.getStudents(courseEntities),
@@ -58,26 +73,39 @@ public class AttendanceRestService {
 
     @GET
     @Path("functions")
-    public List<FunctionEntity> getFunctions(@DefaultValue("1") @QueryParam(value = "orgId") List<BigDecimal> orgIds) {
+    public List<FunctionEntity> getFunctions(
+            @DefaultValue("1") @QueryParam(value = "orgId") List<BigDecimal> orgIds,
+            @HeaderParam(value = "X-Authorization") String secret
+    ) {
+        checkAuthorizationHeader(secret);
         return userService.getFunctions(orgIds);
     }
 
     @GET
     @Path("organisations")
-    public List<OrganisationDto> getOrganisations() {
+    public List<OrganisationDto> getOrganisations(
+            @HeaderParam(value = "X-Authorization") String secret
+    ) {
+        checkAuthorizationHeader(secret);
         return organisationService.getOrganisations();
     }
 
     // slow CO Public views
     @GET
     @Path("studies")
-    public List<StudyEntity> getStudies() {
+    public List<StudyEntity> getStudies(
+            @HeaderParam(value = "X-Authorization") String secret
+    ) {
+        checkAuthorizationHeader(secret);
         return StudyEntity.listAll();
     }
 
     @GET
     @Path("test")
-    public String test() {
+    public String test(
+            @HeaderParam(value = "X-Authorization") String secret
+    ) {
+        checkAuthorizationHeader(secret);
         return "blabla";
     }
 }
