ad15d17c064b0ca054168b77433fe0555dd6743e
Zoltan Sasvari <sasvarizoli@me.com>
2021-11-04 20:46:56 +0100

    added debug mode

--- src/main/java/com/studo/services/attendance/rest/AttendanceTestRestService.java
+++ src/main/java/com/studo/services/attendance/rest/AttendanceTestRestService.java
@@ -6,6 +6,8 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 // disable the user session to allow machine to machine communication
 @UserSessionDisabled
@@ -13,10 +15,29 @@ import javax.ws.rs.Produces;
 @Path("rest/attendance")
 public class AttendanceTestRestService {
 
+  private static final Logger logger = Logger.getLogger(AttendanceTestRestService.class);
+
   @GET
   @Path("hello-world")
   public String helloWorld() {
     return "hello world";
+  }
+
+  @ConfigProperty(name = "studo-service.debug-mode") Boolean debug;
+  @ConfigProperty(name = "studo-service.token-secret") String token_secret;
+  @ConfigProperty(name = "studo-service.backend-client-secret") String client_secret;
+  @ConfigProperty(name = "studo-service.dal-base-url") String dal_base_url;
+
+  @GET
+  @Path("debug")
+  public String debug() {
+    if (!debug) return "Please enable debug mode in the guid.yaml";
+
+    logger.warn("studo-service.token-secret: " + token_secret);
+    logger.warn("studo-service.backend-client-secret: " + client_secret);
+    logger.warn("studo-service.dal-base-url: " + dal_base_url);
+
+    return "Logged ENV vars";
   }
 
   @GET
