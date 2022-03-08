1fc6f47987d8918dcc54fdd6024654d2ca20c8c4
Valentin Slawicek <valentin.slawi@gmail.com>
2022-03-08 13:48:09 +0100

    Add missing fields

--- src/main/java/com/studo/services/attendance/entity/function/FunctionEntity.java
+++ src/main/java/com/studo/services/attendance/entity/function/FunctionEntity.java
@@ -1,6 +1,5 @@
 package com.studo.services.attendance.entity.function;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.studo.services.attendance.entity.staff.StaffFunctionCompositeKey;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import org.hibernate.annotations.Immutable;
@@ -10,6 +9,7 @@ import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * @author Arbër Gjergjizi <arber.gjergjizi@campus02.at>
@@ -25,7 +25,7 @@ public class FunctionEntity extends PanacheEntityBase {
     @Column(name = "ORG_NR", insertable=false, updatable=false)
     public BigDecimal organisationId;
 
-    @Column(name = "PERSON_NR", insertable=false, updatable=false)
+    @Column(name = "PERSON_NR", insertable = false, updatable = false)
     public BigDecimal staffId;
 
     @Column(name = "NAME")
@@ -33,6 +33,15 @@ public class FunctionEntity extends PanacheEntityBase {
 
     @Column(name = "KURZBEZEICHNUNG")
     public String type;
+
+    @Column(name = "ANZEIGE_IN_PERS_LISTE")
+    public String showInPersList; // "J" or "N"
+
+    @Column(name = "BEGINN_DATUM")
+    public Date startDate;
+
+    @Column(name = "ENDE_DATUM")
+    public Date endDate; // Optional
 
     public String getId() {
         return id.toString();
